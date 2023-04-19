e34ea9cfcad20e8e45d22570729047648102a3cf
Nkaka23dev <nkakaeric96@gmail.com>
2023-04-19 11:19:16 +0200

    feat: added a regex exec and test methods

--- app.js
+++ app.js
@@ -1,18 +1,4 @@
-const reverseString = (str) => {
-    if (str.length === 0) {
-        return ""
-    }
-    return reverseString(str.substring(1)) + str.charAt(0)
-}
-console.log(reverseString("Everyone Is Happy Where He is!"))
+const regex = /^hello (\w{3,5})$/i
+const text = "Hello wdo";
 
-/**
-reversTring("") + "a",  =>                "a"
-reversTring("a") + "k",   =>              "a" + "K"
-reversTring("ka") + "a",   =>             "a" + "K" + "a"
-reversTring("aka") + "k",   =>            "a" + "K" + "a" + "K"
-reversTring("kaka") + "N",   =>           "a" + "K" + "a" + "K" + "N"
-
-akakn
-
- * **/
+console.log(regex.exec(text))
