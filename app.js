85e4b2e43246697a79996dc20f3e24741623a0d6
Nkaka23dev <nkakaeric96@gmail.com>
2023-04-28 16:53:54 +0300

    feat: started react d

--- app.js
+++ app.js
@@ -1,34 +1,4 @@
-const add = (num1, num2) => {
-    return num1 + num2
-}
-const mult = (num1, num2) => {
-    return num1 * num2
-}
-const division = (num1, num2) => {
-    return num1 / num2
-}
-const substraction = (num1, num2) => {
-    return num1 - num2
-}
-const operators = ["+", "-", "/", "*"];
+let post = "";
+let jsInput;
+let jsDiv;
 
-const operator = operators[Math.floor(Math.random() * operators.length)];
-
-// console.log(operator)
-
-switch (operator) {
-    case "+":
-        console.log("Numbers Added: ",add(8,8))
-        break
-    case "*":
-        console.log("Multiplication: ",mult(8,8))
-        break
-    case "/":
-       console.log("Division: ",division(8,8))
-        break
-    case "-":
-        console.log("Substraction: ",substraction(8,8))
-        break
-    default:
-        console.log("Operator does not exist")
-}
