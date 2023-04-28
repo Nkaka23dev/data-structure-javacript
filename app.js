df9453ad1723ca1454ff73aed93303ab9c21d1cd
Nkaka23dev <nkakaeric96@gmail.com>
2023-04-28 12:15:11 +0300

    feat: finish switch

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
-
-/*
-1) maximum number to be passed is 359999  seconds
-2) I need to return them in (HH:MM:SS)
-359999 seconds  =  359999/60  = 
-359999%60 ===> 59 seconds  and 5999 minutes
-5999%60 ===>    59 minutes and   99 hours
-
-*/
-// console.log(359999 % 60)
-// console.log(Math.floor(359999 / 60))
-
-const HumanreadableFormat = (seconds) => {
-    if (seconds <= 359999) {
-        let sec = seconds % 60; // 59
-        let remind = Math.floor(seconds / 60)
-        let minutes = remind % 60  //
-        let hours = Math.floor(remind / 60)
-
-        const padNumber = (number) => {
-            return number.toString().length < 2 ? '0' + number : number;
-        }
-        const result = padNumber(hours) + ':' + padNumber(minutes) + ':' + padNumber(sec);
-        return result;
-    }
+const add = (num1, num2) => {
+    return num1 + num2
 }
-console.log(HumanreadableFormat(3999))
+const mult = (num1, num2) => {
+    return num1 * num2
+}
+const division = (num1, num2) => {
+    return num1 / num2
+}
+const substraction = (num1, num2) => {
+    return num1 - num2
+}
+const operators = ["+", "-", "/", "*"];
 
+const operator = operators[Math.floor(Math.random() * operators.length)];
 
+// console.log(operator)
+
+switch (operator) {
+    case "+":
+        console.log("Numbers Added: ",add(8,8))
+        break
+    case "*":
+        console.log("Multiplication: ",mult(8,8))
+        break
+    case "/":
+       console.log("Division: ",division(8,8))
+        break
+    case "-":
+        console.log("Substraction: ",substraction(8,8))
+        break
+    default:
+        console.log("Operator does not exist")
+}
