3776b8ad75ef194943bb1ccb74c4023411d14407
Nkaka23dev <nkakaeric96@gmail.com>
2023-05-24 17:41:01 +0300

    feat: finish closure

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-const reduce2 = (arr, buildUp, acc) => {
-    for (let ar of  arr) {
-        acc = buildUp(acc, ar);
+// const funcCreator = () => {
+//     let count = 0;
+//     const add3 = (num1) => {
+//         let result = num1 + 3;
+//         return result;
+//     }
+//     return add3
+// } 
+
+
+// const generateFunc = funcCreator();
+
+// console.log(generateFunc()) 
+
+const inner = () => {
+    let counter = 0;
+    const incerement = () => {
+        return counter += 1;
     }
-    return acc
+    return incerement;
 }
-const add = (a, b) => {
-    return a + b;
-}
-const sum = reduce2([2, 3, 4,5,6,7,8,8], add, 0);
-console.log(sum)
+
+const toBerun = inner();
+
+console.log(toBerun())
+console.log(toBerun())
+console.log(toBerun())
