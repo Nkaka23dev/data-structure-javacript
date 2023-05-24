d2adeec115e7ce428d48f06cf6eb26bff26b4fdd
Nkaka23dev <nkakaeric96@gmail.com>
2023-05-24 12:07:51 +0300

    feat:

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-let post = "";
-let jsInput;
-let jsDiv;
+const numbers = [{
+    names: "eric",
+    age: 4,
+},
+{
+    names: "Nkaka",
+    age: 3,
+},
+{
+    names: "John",
+    age: 2,
+}
+]
+
+const new_arr = numbers.map((value, index, arr) => {
+    return value.names +'  '+ value.age
+})
+
+console.log(new_arr)
 
