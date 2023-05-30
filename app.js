3dc87f4094ecda556906ae0be7c946aada0eedad
Nkaka23dev <nkakaeric96@gmail.com>
2023-05-30 16:58:48 +0300

    feat: finish adding

--- app.js
+++ app.js
@@ -1,44 +1,22 @@
-
-// Approach one efor creating object(using object literals)
-const user1 = {
-    name: "Nkaka",
-    score: 4,
-    increment: () => this.score += 1
-}
-user1.increment();
-console.log(user1.score) 
-
-
-// Approach two of creating object(Object.create fuction)
-const user3  = Object.create(null);
-
-user3.name = "Nkaka Eric";
-user3.age = 56 
-console.log(user3); 
-
-
-// Approach three is similar to the above and is called object dot notation, define empty object and assign values 
-
-
-// Approach four of creating object(with function)
-const createObjects = (name, score) => {
-    const newUser = {};
+const createUser = (name, score) => {
+    const newUser = Object.create(sharedFunctionalities);
     newUser.name = name;
     newUser.score = score;
-    newUser.increment = () => newUser.score += 1;
-    newUser.decrement = () => newUser.score -= 1;
-    return newUser;
     return newUser;
 }
 
-const userA = createObjects("Jame", 65);
-const userB = createObjects("Eric Nkaka", 87);
+const sharedFunctionalities = {
+    increment: function () {
+        return this.score += 1
+    },
+    login: () => console.log("user Successfully logged in")
+} 
 
-console.log(userA.increment())
-console.log("User B is", userB.increment()) 
+const user1 = createUser("Eric Nkaka", 45);
+const user2 = createUser("kettia", 4);
+console.log(user1.increment())
 
-/**
- Creating of object like obove is not an efficient way of creating object, because increment and decrement functions are repeated whenever we create a new user, which means if we have 1000 users we will have 2000 copy of decrement and increment. this problem is overcomed by use prototype chain, we create an object that stores all functions and each created user can have access on it.
- * **/
-
-
+console.log("Object", user1)
+console.log(user1.name)
+user1.login();
+console.log(user1.score)
