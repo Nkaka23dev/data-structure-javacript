6f128af9628b72e12476349b98b0c9106d112a33
Nkaka23dev <nkakaeric96@gmail.com>
2023-06-05 13:20:06 +0300

    feat: done with some and find

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+// const arr  = [3,4,5,6,7,8];
+
+// console.log(arr.find(element => element === 3) !== undefined);// return true
+// console.log(arr.find(element => element > 6) )// return 6 
+
+//Normally find method return the first element that meet the condition;
+
+// const arr2 = [{ name: "Nkaka", age: 43 }, { name: "James", age: 4 }, { name: "Chris", age: 78 }]; 
+
+// console.log(arr2.find(element => element.name === "Nkaka") !== undefined)
+
+
 const arr  = [3,4,5,6,7,8];
 
-if(arr.includes(3)){
-  console.log("The value exist on this array")
-}
-else{
-  console.log("This value does not exist on this object")
-}
+console.log(arr.some(element => element === 4))
+
