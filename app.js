f60736c256b645301cebc308291978e40d54411a
Nkaka23dev <nkakaeric96@gmail.com>
2023-06-05 13:05:48 +0300

    feat: done with the use of includes

--- app.js
+++ app.js
@@ -1,41 +1,8 @@
-function CreateUser(name, score) {
-    this.name = name;
-    this.score = score;
+const arr  = [3,4,5,6,7,8];
+
+if(arr.includes(3)){
+  console.log("The value exist on this array")
 }
-
-CreateUser.prototype.increment = function () {
-    const add = () => this.score += 1
-    add();
+else{
+  console.log("This value does not exist on this object")
 }
-
-CreateUser.prototype.login = () => console.log("USE LOGIN SUCCESS!")
-
-const user1 = new CreateUser("Eric Nkaka", 76);
-const user2 = new CreateUser("Kettia", 12);
-
-user1.increment()
-// console.log(user1);
-console.log(user1.score);
-
-// const createUser = (name, score) => {
-//     const newUser = Object.create(sharedFunctionalities);
-//     newUser.name = name;
-//     newUser.score = score;
-//     return newUser;
-// }
-
-// const sharedFunctionalities = {
-//     increment: function () {
-//         return this.score += 1
-//     },
-//     login: () => console.log("user Successfully logged in")
-// }
-
-// const user1 = createUser("Eric Nkaka", 45);
-// const user2 = createUser("kettia", 4);
-// console.log(user1.increment())
-
-// console.log("Object", user1)
-// console.log(user1.name)
-// user1.login();
-// console.log(user1.score) 
