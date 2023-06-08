5dc99644fb2d0592aba17529ec1e5982c89ef151
Nkaka23dev <nkakaeric96@gmail.com>
2023-06-08 18:17:23 +0300

    feat: done with global variable

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
-// const arr = new Float32Array([3.544,4,5,2,5,2]);
-
-// console.log(arr) 
 
 
-const buffer = new ArrayBuffer(8)
+// function add(a, b) {
+//     let name = 'Eric Nkaka'
+//     return a + b
+// }
 
-const arr = new Float32Array(buffer);
-arr[0] = 937
-arr[1] = 9
-arr[2] = 7
-arr[3] = 3
-console.log(arr)
+// if(true){
+//     const age = 773765435;
+// }
+// console.log(name)
+
+var name = "nakka"
+var name  = "ert"
+
+
+// console.log(score)
+// // console.log(score)
+
+// let x = 1;
+
+// if(x === 1){
+//     let x = 2;
+//     console.log(x)
+// };
+
+// console.log(x)
+
+// var x = 1;
+
+// if(x === 1){
+//     var x = 2;
+//     console.log(x)
+// };
+
+// console.log(x)
+
+// var age = 8;
+// let score = 9
+
+// var age = 98
+
+// console.log(age)
