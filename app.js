61190f6ee9470adb9e36395e3abbc141139818a2
ericnkaka <nkakaeric96@gmail.com>
2023-08-20 17:36:46 +0200

    feat: done

--- app.js
+++ app.js
@@ -1,18 +1,43 @@
-const reverseString = (input) => {
- if(input === ""){
-  return ""
- } 
- return reverseString(input.substring(1)) +  input.charAt(0)
+function UserCreator(name, score) {
+    this.name = name;
+    this.score = score;
+};
+
+UserCreator.prototype.increment = function () {
+    const add = () => {
+        this.score++;
+    }
+    return add()
 }
 
-/****
- * reversString(ello) + H
- * reversString(llo) + eH
- * reversString(lo) + leH
- * reversString(o) + lleH
- *  + olleH
- *olleH
- * 
- * 
- */
-console.log(reverseString("Hello"))
+UserCreator.prototype.login = function () {
+    console.log("Login")
+}
+
+const user1 = new UserCreator("Nkaka Eric", 10);
+
+console.log(user1.increment())
+
+
+
+// BELOW IS THE SCOND WAY OF CREATING AN OBJECT
+
+// function userCreator(name, score) {
+//     const newUser = Object.create(createUserObj);
+//     newUser.name = name;
+//     newUser.score = score;
+//     return newUser
+// };
+
+// const createUserObj = {
+//     incerement: function () {
+//         return this.score += 1;
+//     },
+//     login: function () {
+//         console.log("Hello Logged In ")
+//     }
+// }
+
+// const user1 = userCreator("Nkaka Eric", 12);
+
+// console.log(user1.incerement())
