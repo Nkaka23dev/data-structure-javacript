0a1a8d80b9c426af36fc0ac158ac6ac071c16f64
ericnkaka <nkakaeric96@gmail.com>
2023-08-21 00:45:34 +0200

    feat: sdd

--- app.js
+++ app.js
@@ -1,43 +1,30 @@
-function UserCreator(name, score) {
-    this.name = name;
-    this.score = score;
-};
-
-UserCreator.prototype.increment = function () {
-    const add = () => {
-        this.score++;
-    }
-    return add()
-}
-
-UserCreator.prototype.login = function () {
-    console.log("Login")
-}
-
-const user1 = new UserCreator("Nkaka Eric", 10);
-
-console.log(user1.increment())
-
-
-
-// BELOW IS THE SCOND WAY OF CREATING AN OBJECT
-
-// function userCreator(name, score) {
-//     const newUser = Object.create(createUserObj);
-//     newUser.name = name;
-//     newUser.score = score;
-//     return newUser
+// function UserCreator(name, score) {
+//     this.name = name;
+//     this.score = score;
 // };
 
-// const createUserObj = {
-//     incerement: function () {
-//         return this.score += 1;
-//     },
-//     login: function () {
-//         console.log("Hello Logged In ")
+// UserCreator.prototype.increment = function () {
+//     // const that = this
+//     // function add() {
+//     //     that.score++;
+//     // }
+//     // add()
+
+//     const add = () => {
+//         this.score++;
 //     }
+//     add()
 // }
 
-// const user1 = userCreator("Nkaka Eric", 12);
+// UserCreator.prototype.login = function () {
+//     console.log("Login")
+// }
 
-// console.log(user1.incerement())
+// const user1 = new UserCreator("Nkaka Eric", 10);
+
+// user1.increment()
+// console.log(user1.score)
+
+console.log()
+
+
