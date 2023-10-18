dd62b6cacc72a6c62ddff6f28bd91f3ca5c29d70
ericnkaka <nkakaeric96@gmail.com>
2023-10-18 09:48:20 +0200

    feat: done

--- app.js
+++ app.js
@@ -1,43 +1,29 @@
-function UserCreator(name, score) {
-    this.name = name;
-    this.score = score;
-};
-
-UserCreator.prototype.increment = function () {
-    const add = () => {
-        this.score++;
-    }
-    return add()
-}
-
-UserCreator.prototype.login = function () {
-    console.log("Login")
-}
-
-const user1 = new UserCreator("Nkaka Eric", 10);
-
-console.log(user1.increment())
-
-
-
-// BELOW IS THE SCOND WAY OF CREATING AN OBJECT
-
-// function userCreator(name, score) {
-//     const newUser = Object.create(createUserObj);
-//     newUser.name = name;
-//     newUser.score = score;
-//     return newUser
-// };
-
-// const createUserObj = {
-//     incerement: function () {
-//         return this.score += 1;
-//     },
-//     login: function () {
-//         console.log("Hello Logged In ")
-//     }
+// function numberEquals(fnumber1, fnumber2) {
+//     return Math.abs(fnumber1 - fnumber2) < Number.EPSILON
 // }
 
-// const user1 = userCreator("Nkaka Eric", 12);
+// console.log(numberEquals(0.1 + 0.2, 0.3))
 
-// console.log(user1.incerement())
+// if (0.1 + 0.2 === 0.3) {
+//     console.log("True")
+// } else {
+//     console.log("False")
+
+
+// const isPrime = (n) => {
+//     if (n <= 1) {
+//         return false
+//     }
+
+//     for (let i = 2; i < n; i++) {
+//         if (n % 2 === 0) {
+//             return false
+//         }
+//     }
+//     return true
+
+// }
+
+// console.log(isPrime(22)) 
+
+console.log(Math.floor(Math.random() * 100))
