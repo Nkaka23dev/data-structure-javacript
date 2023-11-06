5a2f8c1f6ef60bbbf43bdeae2be50d97f3c11e96
ericnkaka <nkakaeric96@gmail.com>
2023-11-06 10:10:06 +0200

    feat: done

--- app.js
+++ app.js
@@ -1,29 +1,88 @@
-// function numberEquals(fnumber1, fnumber2) {
-//     return Math.abs(fnumber1 - fnumber2) < Number.EPSILON
+let array1 = [1, 2, 3, 4];
+
+//push on array always return the length of array
+// console.log(array1.push(6))
+
+//pop method  on array always return a deleted element
+// console.log(array1.pop());
+
+
+//pop method  on array always return a deleted element
+// console.log(array1.shift());
+
+// console.log(array1)
+
+// for (let i = 0; i < array1.length; i++) {
+//     console.log(array1[i])
 // }
 
-// console.log(numberEquals(0.1 + 0.2, 0.3))
+// let counter = 0
 
-// if (0.1 + 0.2 === 0.3) {
-//     console.log("True")
-// } else {
-//     console.log("False")
-
-
-// const isPrime = (n) => {
-//     if (n <= 1) {
-//         return false
-//     }
-
-//     for (let i = 2; i < n; i++) {
-//         if (n % 2 === 0) {
-//             return false
-//         }
-//     }
-//     return true
-
+// while (counter < array1.length) {
+//     console.log("Printed inside loop", array1[counter]);
+//     counter++
 // }
 
-// console.log(isPrime(22)) 
+// let array2 = ['all', 'cows', 'are', 'big'];
 
-console.log(Math.floor(Math.random() * 100))
+// array3 = Array.from(array2)
+
+// array3[0] = "New added element"
+
+// console.log(array3, "Copy of array");
+// console.log(array2, "Original array")
+
+// console.log(array2.slice(-3), "Sliced array");
+
+
+// const myCopy = array2.slice();
+// myCopy.push("Pushed Item to the original array")
+
+// console.log(array2, "My original Array is here!")
+// console.log(myCopy, "Copy is stored right here")
+
+// for (let i in array2) {
+//     console.log(array2[i], "------>Printed using for in")
+// }
+
+// for (let item of array2) {
+//     if (item === 'cow') {
+//         continue
+//     }
+//     console.log(item, "------>Printed using for of")
+// } 
+
+// array2.forEach((value, index) => {
+
+//     console.log(array2[index], index)
+// })  
+
+let array2 = ['all', 'cows', 'are', 'big'];
+
+// console.log(array2.length)
+// let array3 = array2.splice(array2.length / 2, 1, "Nkaka");
+// console.log(array2, "Original array")
+// console.log(array3) 
+
+// console.log(array2.splice(0, 0, { name: "Nkaka", age: 56 }))
+
+// console.log(array2[0].name)
+
+// array2 = array2.concat("Nkaka Eric");
+// console.log(array2);
+
+// array2.length = 3
+// console.log(array2)
+
+// console.log(...array2) 
+// const myNewArray = [3, 98, 212, 43, 23, 54];
+// console.log(Math.min(...myNewArray)) 
+
+let arr = [1, 2, 3, 4, 5];
+// console.log(arr);
+
+console.log(arr)
+
+//This is the function that receive  list of numbers and a weight, a weight might be any number
+// const findSum = (list, weight) => {
+// }
